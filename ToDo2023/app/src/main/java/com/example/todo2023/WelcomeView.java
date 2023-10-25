2680d02bc5f3915db8d7076cf94c0f809f0cf4fd
genefu <genesis_fu@yahoo.com>
2023-10-25 17:22:43 -0400

    Adds welcome view

---
+++ ToDo2023/app/src/main/java/com/example/todo2023/WelcomeView.java
@@ -0,0 +1,14 @@
+package com.example.todo2023;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_view);
+    }
+}
