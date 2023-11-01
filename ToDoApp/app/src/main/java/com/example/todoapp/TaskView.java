252ba83afa34d00bd042f7a3f2cc1253e8fe06b1
genesis <116662964+genefu@users.noreply.github.com>
2023-11-01 13:32:25 -0400

    Adds task view and button

--- ToDoApp/app/src/main/java/com/example/todoapp/TaskView.java
+++ ToDoApp/app/src/main/java/com/example/todoapp/TaskView.java
@@ -1,6 +1,8 @@
 package com.example.todoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,9 @@ public class TaskView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasks_view);
+    }
+
+    public void toWelcome(View view) {
+        startActivity(new Intent(TaskView.this, WelcomeView.class));
     }
 }
